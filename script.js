60e5986fed9e7f993d521193102a2d2090819036
Hugo <huwgoma@gmail.com>
2021-04-19 19:55:32 -0400

    First iteration of conditional RPS handling function

--- script.js
+++ script.js
@@ -1,37 +1,68 @@
-//Generate a random number between 1-3
-function computerRNG(){
-    let randomNumber=Math.floor(Math.random()*(3-1+1)+1);
-    return randomNumber;
-}
-//Call computerRNG, then assign a string to compRPS based on the randomly-generated number
-function computerRPS(){
-    let compNum=computerRNG();
-    console.log(compNum)
-    let compRPS=''
-    if (compNum==1){ //RNG 1 = Rock
-        compRPS='Rock';
-        
-        console.log(compRPS);
-    } else if (compNum==2){ //RNG 2 = Paper
-        compRPS='Paper';
-
-        console.log(compRPS);
-    } else if (compNum==3){ //RNG 3 = Scissors
-        compRPS='Scissors'
-
-        console.log(compRPS);
-    } else {
-        let errorMessage='Sorry, an error occurred.'
-        console.log(errorMessage);
-    }
-}
-
 //Get user input (Rock, Paper, or Scissors) 
 function getUserRPS(){
     let userInput = prompt('Please enter Rock, Paper, or Scissors:', '');
     userInput=userInput.toLowerCase(); // Convert to lowercase
     let firstLetter=userInput.charAt(0); // Get the first letter of user input
     userInput=userInput.replace(firstLetter, firstLetter.toUpperCase());
+
+    console.log('Player: ' + userInput);
+    return userInput;
 }
-getUserRPS();
-computerRPS();
+
+//Computer: Generate a random number between 1-3
+function computerRNG(){
+    let randomNumber=Math.floor(Math.random()*(3-1+1)+1);
+    return randomNumber;
+}
+//Call computerRNG, then assign a string to compRPS based on the randomly-generated number
+function computerRPS(compNum){
+    compNum=computerRNG();
+    console.log(compNum)
+    let compRPS=''
+    if (compNum==1){ //RNG 1 = Rock
+        compRPS='Rock';
+        
+        console.log('Computer:' + compRPS);
+        return compRPS;
+    } else if (compNum==2){ //RNG 2 = Paper
+        compRPS='Paper';
+
+        console.log('Computer:' + compRPS);
+        return compRPS;
+    } else if (compNum==3){ //RNG 3 = Scissors
+        compRPS='Scissors'
+
+        console.log('Computer:' + compRPS);
+        return compRPS;
+    } else {
+        let errorMessage='Sorry, an error occurred.'
+        console.log(errorMessage);
+    }
+}
+
+
+
+
+function displayResult(user, computer){
+    console.log('display');
+    let result=''
+
+    switch(true){
+        //Loss
+        case (user=='Rock' && computer=='Paper'):
+        case (user=='Paper' && computer =='Scissors'):
+        case (user=='Scissors' && computer == 'Rock'):
+            result=`${computer} beats ${user}, you lose!`;
+            console.log(result);
+            break;
+        //Win
+        case (user=='Rock' && computer=='Scissors'):
+        case (user=='Paper' && computer =='Rock'):
+        case (user=='Scissors' && computer == 'Paper'):
+            result=`${user} beats ${computer}, you win!`;
+            console.log(result);
+            break;
+    }
+}
+
+displayResult(getUserRPS(), computerRPS());
