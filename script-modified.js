5c07df41d6bfb7dd4665c16dcca0b38bec741f70
Hugo <huwgoma@gmail.com>
2021-04-29 20:52:05 -0400

    First working iteration of RPS with the button; also reorganized function structures

--- script-modified.js
+++ script-modified.js
@@ -1,15 +1,21 @@
 //Play button
 let submitBtn=document.querySelector(`button[id='submitBtn']`);
-submitBtn.addEventListener('click', getUserRPS);
+submitBtn.addEventListener('click', playGame);
 
 
-
+function playGame(){
+    getUserRPS();
+    getComputerRPS();
+    winLossCount();
+}
 //Get user input (Rock, Paper, or Scissors) 
 function getUserRPS(){
     let rpsChoices=document.querySelectorAll(`input[name='userRPSChoice']`);
     for(i=0;i<rpsChoices.length;i++){
         if(rpsChoices[i].checked){
             let userRPSChoice=rpsChoices[i].getAttribute('value');
+            let userRPSChoiceFirst=userRPSChoice.charAt(0);
+            userRPSChoice=userRPSChoice.toLowerCase().replace(userRPSChoiceFirst, userRPSChoiceFirst.toUpperCase());
             console.log (userRPSChoice);
             return userRPSChoice;
         }
@@ -25,8 +31,8 @@ function computerRNG(){
 }
 
 //Call computerRNG, then assign a string to compRPS based on the randomly-generated number
-function computerRPS(compNum){
-    compNum=computerRNG();
+function getComputerRPS(){
+    let compNum=computerRNG();
     let compRPS=''
     //RNG 1 = Rock
     if (compNum==1){ 
@@ -91,53 +97,19 @@ function displayResult(user, computer){
 
     }
 }
-
-
-
-//Game looping function; play 5 times
-function playGame(){
+//Function for counting Wins/Losses
+function winLossCount(){
     let userWinCount=0;
     let userLossCount=0;
-    //Function for counting Wins/Losses
-    let winLossCount = function(result){
-        if(result=='Win'){
-            userWinCount++;
-            return userWinCount;
-        } else if(result=='Loss'){
-            userLossCount++;
-            return userLossCount;
-        }
-    }
+    let gameResult=displayResult(getUserRPS(), getComputerRPS());
+    console.log(gameResult);
     
-    //Function for comparing Win/Loss count and determining overall winner
-    function winLossCompare(userWins, userLosses){
-        let matchResult='';
-        
-        if (userWins>userLosses){
-            return matchResult=`Congratulations, you win ${userWins}-${userLosses}!`;
-        } else if (userWins<userLosses){
-            return matchResult=`Computer wins ${userLosses}-${userWins}.`;
-        } else if (userWins==userLosses){
-            return matchResult=`It's a tie!`;
-        }
+    if(gameResult=='Win'){
+        userWinCount++;
+        return userWinCount;
+    } else if(gameResult=='Loss'){
+        userLossCount++;
+        return userLossCount;
     }
-
-    //For loop, loop the game 5 times
-    function gameLoop(){
-        for(i=1;i<=5;i++){
-            let result=displayResult(getUserRPS(), computerRPS());
-            console.log(result);
-            winLossCount(result);
-            console.log('Your Wins: '+ userWinCount);
-            console.log(`Computer's Wins: ${userLossCount}`);
-            //Run displayResult (Win/Loss handling), then pass the result to winLossCount
-            //This should return a count of your wins and losses over the 5 games 
-        }
-        //after loop is done, compare wins and losses to determine match winner
-        //Log the match result
-        console.log(winLossCompare(userWinCount, userLossCount));
-    }
-    gameLoop();
-    
 }
 
